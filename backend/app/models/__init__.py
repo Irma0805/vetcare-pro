b21cc95378e51e3c24100d4b3d2d77e6bffe1116
Irma0805 <irmajortizs@hotmail.es>
2026-07-09 14:40:19 +0200

    feat: initialize Alembic and generate first migration for complete SQL schema

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.models.propietario import Propietario
+from app.models.veterinario import Veterinario
+from app.models.tratamiento import Tratamiento
+from app.models.mascota import Mascota
+from app.models.cita import Cita
+from app.models.factura import Factura
+from app.models.citas_tratamientos import CitasTratamientos
